6509f0c941ee421b57b53637c93db1fee9748fe0
Raul Medeiros <raul.medeiros@vortigo.com.br>
2022-11-11 13:45:11 -0300

    Add Class Author

---
+++ Rocket Reviews/Author.swift
@@ -0,0 +1,17 @@
+//
+//  Author.swift
+//  Rocket Reviews
+//
+//  Created by Raul de Medeiros on 11/11/22.
+//
+
+import Foundation
+
+
+class Author{
+    let name : String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
