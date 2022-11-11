badc89937631b2ab01dcd7578016cbc445b9bfb3
Raul Medeiros <raul.medeiros@vortigo.com.br>
2022-11-11 14:01:43 -0300

    Add Authors and Media

--- Rocket Reviews/Author.swift
+++ Rocket Reviews/Author.swift
@@ -5,13 +5,23 @@
 //  Created by Raul de Medeiros on 11/11/22.
 //
 
-import Foundation
+import UIKit
 
 
 class Author{
     let name : String
-    
-    init(name: String) {
+    var avatar : UIImage
+    let description : String
+    let github : String
+    let instagram : String
+    let linkedin : String
+
+    init(name: String, avatar: UIImage, description: String, github: String = "sem", instagram: String = "sem", linkedin: String = "sem") {
         self.name = name
+        self.avatar = avatar
+        self.description = description
+        self.github = github
+        self.instagram = instagram
+        self.linkedin = linkedin
     }
 }
